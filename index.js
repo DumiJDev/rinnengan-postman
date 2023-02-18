29b7c89b6c1a6976b1afde1b9f282764d96e7ef3
Dumildes Paulo <dumildesfernando@outlook.com>
2023-02-18 21:31:37 +0100

    feat: add form and actions

---
+++ index.js
@@ -0,0 +1,72 @@
+const swagger = document.getElementById('swagger')
+const btnLimpa = document.getElementById('limpa')
+const btnGerar = document.getElementById('gerar')
+const resultados = document.getElementById('content')
+
+const print = console.log
+console.log = undefined
+
+const operations = []
+
+function limpar() {
+  swagger.value = ''
+  resultados.innerHTML = ''
+  operations.splice(0, operations.length)
+}
+
+btnLimpa.addEventListener('click', limpar)
+
+const keys = (o) => Object.keys(o)
+
+const indices = (paths) => {
+  const keys1 = []
+
+  for (const el of keys(paths)) {
+
+    let key = { name: el, methods: [] }
+
+    for (let method of keys(paths[el])) {
+      if (method !== 'parameters')
+        key.methods.push(method)
+    }
+
+    keys1.push(key)
+  }
+
+  return keys1
+}
+
+const popula = (name, status = 200, schema = []) => {
+  operations.push({ name, status, schema })
+}
+
+const desenhaLista = () => {
+  resultados.innerHTML = operations.map(o1 => `<div class="operation"><div>${o1.name.length < 31 ? o1.name : o1.name.substring(0, 30) + '...'}</div><button id="imprimir">desc</button></div>`).join('')
+}
+
+function lerSwagger() {
+  const paths = JSON.parse(swagger.value).paths
+
+  const indexes = indices(paths)
+
+  for (let i of indexes)
+    for (let m of i.methods) {
+      const op = paths[`${i.name}`][`${m}`]
+
+      for (let o of keys(op.responses)) {
+        if (op.responses[o].examples)
+          popula(op.operationId,
+            parseInt(o),
+            [...keys(op.responses[o].examples).map(key => op.responses[o].examples[key])]
+          )
+        else
+          popula(op.operationId, parseInt(o))
+      }
+
+    }
+
+  print(operations)
+  desenhaLista()
+}
+
+btnGerar.addEventListener('click', lerSwagger)
